c9bb21ba6802659eb8c30d93332cff875bdfb5d8
Neeraj Bhardwaj <43618574+nebhardw@users.noreply.github.com>
2025-03-18 13:53:23 -0400

    Create JavaTest.java

---
+++ JavaTest.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class JavaTest {
+    String passphrase = RandomStringUtils.randomAlphanumeric(32);
+    public testOnly {
+        return 1;
+}
+}
